8ded2d1919746cf62e54f5ed8147db205ea4ca0f
Assis-Mohanty <assis.mohanty.98@gmail.com>
2025-02-27 16:21:37 +0530

    basic setup

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages,setup
+
+setup (
+    name="Legal Aid Chatbot",
+    version="0.0.0",
+    author="Bharat Ali",
+    author_email="bharatAliHacker@hacker.com",
+    pakages=find_packages(),
+    install_requires=[]
+)
